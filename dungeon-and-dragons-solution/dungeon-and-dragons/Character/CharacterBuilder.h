c8022a0ff36292ded4ed7ab6906f27f58e2f9346
Lance Lafontaine <lance.lafontaine92@gmail.com>
2016-11-29 20:08:02 -0500

    Adds all files to new directory

---
+++ dungeon-and-dragons-solution/dungeon-and-dragons/Character/CharacterBuilder.h
@@ -0,0 +1,45 @@
+#ifndef CHARACTER_BUILDER_H
+#define CHARACTER_BUILDER_H
+#include "Fighter.h"
+#include "../Dice/Dice.h"
+#include "CharacterAttr.h"
+using namespace std;
+
+class CharacterBuilder {
+
+public:
+	CharacterBuilder(string fighterType, int level);
+	Character* getCharacter();
+	void createCharacter();
+	virtual void setPlayerType() = 0;
+
+protected:
+	Fighter* c_character;
+	string fighterType;
+	int fighterLevel;
+};
+
+class PlayerCharacterBuilder : public CharacterBuilder {
+
+public:
+	PlayerCharacterBuilder(string fighterType, int level);
+	void setPlayerType();
+
+};
+
+class EnermyCharacterBuilder : public CharacterBuilder {
+
+public:
+	EnermyCharacterBuilder(string fighterType, int level);
+	void setPlayerType();
+
+};
+class FriendlyCharacterBuilder : public CharacterBuilder {
+
+public:
+	FriendlyCharacterBuilder(string fighterType, int level);
+	void setPlayerType();
+
+};
+
+#endif
---
+++ dungeon-and-dragons-solution/dungeon-and-dragons/Character/CharacterGenerator.cpp
@@ -0,0 +1,14 @@
+#include "CharacterGenerator.h"
+
+void CharacterGenerator::setCharacterBuilder(CharacterBuilder* cb) {
+	this->c_characterBuilder = cb;
+};
+
+Character* CharacterGenerator::getCharacter() {
+	return this->c_characterBuilder->getCharacter();
+}
+
+void CharacterGenerator::createCharacter() {
+	c_characterBuilder->createCharacter();
+        c_characterBuilder->setPlayerType();
+};
