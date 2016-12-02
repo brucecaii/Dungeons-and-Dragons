718c3789beddd8b60fbfb6301ade0c542f5f8465
Zhipeng Bruce Cai <rkevincai@hotmail.com>
2016-12-02 02:10:50 -0500

    update new equip logic

--- dungeon-and-dragons-solution/dungeon-and-dragons/Driver.cpp
+++ dungeon-and-dragons-solution/dungeon-and-dragons/Driver.cpp
@@ -84,26 +84,27 @@ int main(int argc, char* argv[]) {
 
 	//equip from backpack 
 	cout << endl << endl << endl << endl << "equip test!!!!!!!!!!!!!" << endl;
-	Enhancement *randomBuff = new Enhancement("con", 4);
-	Item *helmet1 = new Item("Helmet", *randomBuff, "super mario hat");
-	Item *helmet2 = new Item("Helmet", *randomBuff, "super benny hat");
+	Enhancement *randomBuff1 = new Enhancement("con", 4);
+	Enhancement *randomBuff2 = new Enhancement("int", 5);
+	Item *helmet1 = new Item("Helmet", *randomBuff1, "super mario hat");
+	Item *helmet2 = new Item("Helmet", *randomBuff2, "super benny hat");
 	you->addItemBackpack(*helmet1);
 	you->addItemBackpack(*helmet2);
+	you->displayCharacterInfo();
 	you->displayCharacterEquipment();
 	you->displayCharacterBackpack();
 	cout << "~~~~~~~~~~~~~~~~~~~~~~~" << endl<< "ok lets try equip from backpack" << endl << "~~~~~~~~~~~~~~~~~~~~~~~" << endl;
 	you->equipItem(you->getCharacterBackpack()->getItemByName("super mario hat"));
-	you->removeItemfromBackpack("super mario hat");
+	you->displayCharacterInfo();
 	you->displayCharacterEquipment();
 	you->displayCharacterBackpack();
 	//equip from backpack and replace existing
 	cout << "~~~~~~~~~~~~~~~~~~~~~~~" << endl << "ok lets try equip from backpack and replace the existing one" << endl << "~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-	you->addItemBackpack(you->getCharacterEquipment()->getItemByName("super mario hat"));
-	you->deEquipItem(you->getCharacterEquipment()->getItemByName("super mario hat").getType());
 	you->equipItem(you->getCharacterBackpack()->getItemByName("super benny hat"));
-	you->removeItemfromBackpack("super benny hat");
+	you->displayCharacterInfo();
 	you->displayCharacterEquipment();
 	you->displayCharacterBackpack();
+
 
 
 	//Items for testing purpose
